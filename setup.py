76196f6fce52dd8fa50919ccf7636ffda7d002a7
Chetan Birajdar <birajdar.chetan@gmail.com>
2017-12-11 20:16:29 -0800

    updated setup.py to list supported python versions

--- setup.py
+++ setup.py
@@ -3,23 +3,30 @@ from setuptools import setup
 setup(
     name='cricket-cli',
     description='Command-line interface for cricket fans',
-    version='0.1',
+    version='1.0.0',
     packages=['cricket'],
     url='http://github.com/cbirajdar/cricket-cli',
     author='Chetan Birajdar',
     author_email='birajdar.chetan@gmail.com',
     license='MIT',
     classifiers=[
-        'Development Status :: Beta',
+        'Development Status :: 1.0.0 - Beta',
         'Intended Audience :: Developers',
-        'License :: MIT'
+        'Natural Language :: English',
+        'License :: MIT',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 2.8',
+        'Programming Language :: Python :: 2.9',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ],
     install_requires=[
         'grequests',
         'feedparser',
         'beautifulsoup4',
-        'terminaltables',
-        'colorclass'],
+        'terminaltables'
+    ],
     entry_points={
         "console_scripts": ['cricket = cricket.__main__:main'],
     }
